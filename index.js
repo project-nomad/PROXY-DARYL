b8d40a6a0bffcd6c0b9a581552b3dee7e26569b3
Daryl Lin <daryllin5@gmail.com>
2018-06-16 14:44:36 -0700

    Changed redirects to AWS/RDS

--- index.js
+++ index.js
@@ -11,40 +11,40 @@ app.use('/listings/:listingId', express.static(path.join(__dirname, 'public')));
 // for booking module port 3001
 
 app.get('/listings/:listingId/booking/core', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`);
+  res.redirect(`http://ec2-54-67-69-199.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
 app.get('/listings/:listingId/booking/availability', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`);
+  res.redirect(`http://ec2-54-67-69-199.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
 app.get('/listings/:listingId/booking/pricing/', (req, res) => {
-  res.redirect(`http://localhost:3001${req.url}`);
+  res.redirect(`http://ec2-54-67-69-199.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
 // for description stuff: port 3002
 
 app.get('/listings/:listingId/overview', (req, res) => {
-  res.redirect(`http://localhost:3002${req.url}`);
+  res.redirect(`http://awslistingmodule-env.x9r2zr55dh.us-west-2.elasticbeanstalk.com${req.url}`);
 });
 
 app.get('/listings/:listingId/arrangements', (req, res) => {
-  res.redirect(`http://localhost:3002${req.url}`);
+  res.redirect(`http://awslistingmodule-env.x9r2zr55dh.us-west-2.elasticbeanstalk.com${req.url}`);
 });
 
 // for reviews stuff: port 3003
 
 app.get('/listings/:listingId/overviews', (req, res) => {
-  res.redirect(`http://localhost:3003${req.url}`);
+  res.redirect(`http://ec2-54-219-133-3.us-west-1.compute.amazonaws.com${req.url}`);
 });
 
 app.get('/listings/:listingId/reviews', (req, res) => {
-  res.redirect(`http://localhost:3003${req.url}`);
+  res.redirect(`http://ec2-54-219-133-3.us-west-1.compute.amazonaws.com${req.url}`);
 });
 // for carousel port 3004
 
 app.get('/listings/:listingId/pictures', (req, res) => {
-  res.redirect(`http://localhost:3004${req.url}`);
+  res.redirect(`http://ec2-18-208-170-149.compute-1.amazonaws.com${req.url}`);
 });
 
 
